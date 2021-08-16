3a326ac0fcc89992fa745b33b514e253ef40ffc5
Jaiber Flor <jaiver.navia@gmail.com>
2021-08-16 11:15:29 -0500

    menu ok

--- landing.js
+++ landing.js
@@ -3,6 +3,7 @@ const navMenu = document.querySelector('.nav__menu');
 const closeNav = document.querySelector('.nav__close');
 const itemNav = document.querySelectorAll('.nav__item--active');
 
+
 navToggle.addEventListener('click', () =>{
     navMenu.classList.toggle('nav__menu-visible')
 })
@@ -11,8 +12,7 @@ closeNav.addEventListener('click',()=>{
     navMenu.classList.remove('nav__menu-visible')
 })
 
-/*navClose.addEventListener('click', ()=>{
+itemNav.forEach(n => n.addEventListener('click', removeNav))
+function removeNav() {
     navMenu.classList.remove('nav__menu-visible')
-})*/
-
-   
+}
